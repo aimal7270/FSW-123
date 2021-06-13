fb9ef949ed5535bf904ceb2b44fe9736cf4352b9
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2021-06-13 01:21:58 -0400

    week-1

---
+++ week-1/app/src/week-1/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todos from './todoList1.js'
+import './Todo.css'
+
+function App(){
+    return(
+        <div>
+        <div id="title">My First ToDo List</div>
+        <Todos />
+        </div>
+    )
+}
+
+export default App
