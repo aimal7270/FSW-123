3c3b12b85a67b5a4a3b13f214600466955444303
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2021-06-21 21:41:53 -0400

    todoList

--- week-2/boxes-with-pros/src/week-2/TodoList2/store2.js
+++ week-2/boxes-with-pros/src/week-2/TodoList2/store2.js
@@ -1,8 +1,8 @@
 const todos = [
-    { text: "fold laundry"},
-    { text: "order E's school books"},
-    { text: "learn it's"},
-    { text: "pack the curtains"}
+    { text: "Finish school work"},
+    { text: "time is running out"},
+    { text: "Cricket game this sunday"},
+    { text: "don't forget to get groceries"}
 ];
 
 export default todos
